2d18dfecc8c123d4c4d2ce18ea8032cdb7f67b89
Saurav0094 <sauravyadav123@gmail.com>
2026-04-13 12:22:39 +0530

    Fix: transaction history now queries transact table directly

--- Online Banking App Spring Boot/src/main/java/com/beko/DemoBank_v1/repository/TransactHistoryRepository.java
+++ Online Banking App Spring Boot/src/main/java/com/beko/DemoBank_v1/repository/TransactHistoryRepository.java
@@ -9,9 +9,9 @@ import java.util.List;
 
 public interface TransactHistoryRepository extends CrudRepository<TransactionHistory, Integer> {
 
-    @Query(value = "SELECT t.* FROM v_transaction_history t JOIN account a ON t.account_id = a.account_id WHERE a.user_id = :user_id", nativeQuery = true)
+    @Query(value = "SELECT t.* FROM transact t JOIN account a ON t.account_id = a.account_id WHERE a.user_id = :user_id", nativeQuery = true)
     List<TransactionHistory> getTransactionRecordsById(@Param("user_id") String user_id);
 
-    @Query(value = "SELECT * FROM v_transaction_history WHERE account_id = :account_id", nativeQuery = true)
+    @Query(value = "SELECT * FROM transact WHERE account_id = :account_id", nativeQuery = true)
     List<TransactionHistory> getTransactionRecordsByAccountId(@Param("account_id") int account_id);
 }
